85daeb2328242d8ebe51a6dc6b27bca7a3158e31
Vasia <vasiapupkin682@gmail.com>
2026-04-18 21:48:50 +0300

    fix(install): update to v0.1.1-testnet binary

--- scripts/install-validator.sh
+++ scripts/install-validator.sh
@@ -4,7 +4,7 @@ set -e
 CHAIN_ID="portalchain"
 MONIKER=${MONIKER:-"my-validator"}
 SEEDS=${SEEDS:-""}
-VERSION="v0.1.0-testnet"
+VERSION="v0.1.1-testnet"
 REPO="vasiapupkin682/portalchain"
 
 echo "=== Installing PortalChain Validator ==="
